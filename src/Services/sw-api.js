8abfd862ecf1e57c4996502bcb0d0a9147ba5a20
kiibra <kibra99@gmail.com>
2024-05-04 14:34:04 -0400

    obtain all starships from api and render in a clickable card and stub up starshipPage

--- src/Services/sw-api.js
+++ src/Services/sw-api.js
@@ -1,7 +1,11 @@
-const baseUrl = 'https://swapi.dev/api/'
+const baseUrl = 'https://swapi.dev/api'
 
 //Obtain all the starships from the API 
 export async function getAllStarships(){
-  const res = await fetch(`${baseUrl}/api/startships`)
+  const res = await fetch(`${baseUrl}/starships`)
+  return res.json()
+}
+export async function getStarship(idx){
+  const res = await fetch(`${baseUrl}/starships/${idx}`)
   return res.json()
 }
